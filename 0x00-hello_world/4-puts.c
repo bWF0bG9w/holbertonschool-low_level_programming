a6cb7d748ed8025f979fba687b23bd4123ca99be
Matt Lopez <1833@holbertonschool.com>
2020-06-11 20:43:26 +0000

    changes to 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
+
 int main(void)
 {
-
-puts("Programming is like building a mulilingual puzzle")\n;
-return(0);
-
+puts("\"Programming is like building a mulilingual puzzle");
+return (0);
 }
