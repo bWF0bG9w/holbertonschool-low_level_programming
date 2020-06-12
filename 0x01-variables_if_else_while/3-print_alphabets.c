cf9f02be10a8d1c7f59c760da1cde83a94d03dae
Matt Lopez <1833@holbertonschool.com>
2020-06-12 23:53:52 +0000

    adding 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <uunistd.h>
+
+int _putchar(char a)
+{
+	return (write(1, &c, 1));
+}
+
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c = c + 1;
+	}
+	c = 'A';
+	while (c <= 'Z')
+	{
+		_putchar(c);
+		c = c + 1;
+	}
+	write('\n');
+	return (0);
+}
