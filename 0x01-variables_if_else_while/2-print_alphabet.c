988dee8647aaaa9773597ccecf6b868a58da882c
Matt Lopez <1833@holbertonschool.com>
2020-06-12 23:05:42 +0000

    edits

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,28 @@
+#include <unistd.h>
+
+/**
+ *main - prints the alphabet in lowercase
+ *Return: 0
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while (c <= 'z')
+	{
+		if (c != 'e' && c != 'q')
+		{
+			_putchar(c);
+		}
+		c = c + 1;
+	}
+	_putchar('\n');
+	return (0);
+}
