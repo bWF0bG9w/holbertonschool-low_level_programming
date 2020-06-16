a59970718e627e47e7d37747e3fc47f2818c5fc1
Matt Lopez <1833@holbertonschool.com>
2020-06-16 23:45:47 +0000

    adding 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * _isalpha - checks for alphabetic characters
+ * @c: test character
+ * Return: 1 if true, 0 if false
+ */
+
+int _isalpha(int c)
+{
+	if (c <= 'Z' &&  c >= 'a')
+	return (1);
+	else
+		return (0);
+}
