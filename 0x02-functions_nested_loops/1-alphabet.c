398f7df14b80f332c934c3cce0b811cd4f95eef5
Matt Lopez <1833@holbertonschool.com>
2020-06-16 17:48:01 +0000

    edits to 0 and header file

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+
+/**
+ * main - prints the alphabet in lowercase
+ * Return: 0
+ */
+
+void print_alphabet(void);
+{
+int a
+	for (a = 'a'; a <= 'z'; a++)
--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,12 +1,1 @@
-/**
- *my holberton header file
- *contains operations and fuctions for
- *holberton coding
- */
-
-#ifndef _PUTCHAR
-#define _PUTCHAR
-
-int _putchar
-
-#endif
+int _putchar(char c);
