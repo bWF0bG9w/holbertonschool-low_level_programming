d93f9f75908a9976d7dde8d7a0c6e84dcfa31668
Matt Lopez <1833@holbertonschool.com>
2020-06-19 00:47:34 +0000

    adding 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - prints numbers 0-9
+ */
+
+void print_numbers(void)
+{
+	int a;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
