874dc096112a72785c30f2a7ee7714d64782662c
Matt Lopez <1833@holbertonschool.com>
2020-06-23 20:11:27 +0000

    adding 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * swap_int - swaps the value of two integers
+ * @a: integer one
+ * @b: integer two
+ * Return: nothing
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
