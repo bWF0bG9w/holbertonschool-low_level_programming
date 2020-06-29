a79fa4981bc4f26c1d26a2ac70aaf5d9781e9485
Matt Lopez <1833@holbertonschool.com>
2020-06-29 20:08:52 +0000

    edits to 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,9 +1,12 @@
-#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+/**
+ * main - prints alphabet
+ *
+ * Return: 0
+ */
+
 
 int main(void)
 {
