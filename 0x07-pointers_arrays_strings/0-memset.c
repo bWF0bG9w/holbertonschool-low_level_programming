9d6c433bbd2753bc26289b12aabf12e8e4873fd7
Matt Lopez <1833@holbertonschool.com>
2020-06-30 14:41:10 +0000

    adding 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * _memset - fills memmory with a constant byte
+ * @s: given string
+ * @b: char
+ * @n: number of bytes
+ * Return: string
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+
+	unsigned int length = 0;
+
+	while (length < n)
+	{
+		s[length] = b;
+	}
+
+	return (s);
+}
