6fdcd9f9307fbc0122ed34e3860fab746384391c
Matt Lopez <1833@holbertonschool.com>
2020-07-04 21:22:31 +0000

    adding 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * main - prints number of arguments passed to it
+ * @argc: argument input
+ * @argv: arrays
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	argv = argv;
+	printf("%d/n", argc - 1);
+	return (0);
+}
