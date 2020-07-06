f82669bc3f4258ff62cda88959a1422a04f428af
Matt Lopez <1833@holbertonschool.com>
2020-07-06 01:05:02 +0000

    adding 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints code name
+ * @argc: inputs
+ * @argv: arrays
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	argc = argc;
+	printf("%s\n", *argv);
+	return (0);
+}
