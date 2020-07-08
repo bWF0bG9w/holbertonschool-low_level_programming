69ab807fc27aababdd052327cc65abf36262840a
Matt Lopez <1833@holbertonschool.com>
2020-07-08 19:00:13 +0000

    adding 5

---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,44 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * argstostr - concats all arguments
+ * @ac: argumnets
+ * @av: strings to concat
+ *
+ * Return: new string
+ */
+
+char *argstostr(int ac, char **av)
+{
+	int a, b;
+	int k = 0, len = 0;
+	char *cat;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (a = 0; a < ac; a++)
+	{
+		while (av[a][len] != 0)
+		{
+			len++;
+		}
+	}
+	len++;
+	cat = malloc(sizeof(char) * len);
+	for (a = 0; a < ac; a++)
+	{
+		while (av[a][b] != 0)
+		{
+			cat[k] = av[a][b];
+			k++;
+			b++;
+		}
+		cat[k] = '\n';
+		k++;
+		b = 0;
+	}
+	cat[k] = '\0';
+	return (cat);
+}
