d6169672e9d12d4aec15d91e2f233dac3fd6cfe3
Matt Lopez <1833@holbertonschool.com>
2020-07-14 19:09:49 +0000

    adding 5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *
+ *
+ *
+ *
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+
+	if(d->name)
+		free(d->name);
+	if (d->owner)
+		free(d->owner);
+	free(d);
+}
