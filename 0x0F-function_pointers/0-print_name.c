141eac94d420f1366bfacf46bda6266da2350c46
Matt Lopez <1833@holbertonschool.com>
2020-07-15 20:07:41 +0000

    esit to 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,6 +2,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * print_name - prints a name
+ * @name: pointer to name
+ * @f: pointer to func
+ * Return: nothing
+ */
+
+
 void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL)
