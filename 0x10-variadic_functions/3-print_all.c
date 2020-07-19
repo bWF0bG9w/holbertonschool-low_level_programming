f5c0af3df46c7e01dc441a0512b1bc9b11616781
Matt Lopez <1833@holbertonschool.com>
2020-07-19 19:23:24 +0000

    adding 3

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,76 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * printc - prints a char
+ * @anything: char
+ * @s: pointer
+ * Return: nothing
+ */
+
+void printc(va_list anything, char *s)
+{
+	printf("%s%c", s, va_arg(anything, int));
+}
+
+/**
+ * printi - prints an integer
+ * @anything: int
+ * @s: pointer
+ * Return: nothing
+ */
+
+void printi(va_list anything, char *s)
+{
+	printf("%s%i", s, va_arg(anything, int));
+}
+
+/**
+ * print_f - prints a float
+ * @anything: float
+ * @s: pointer
+ * Return: nothing
+ */
+
+void print_f(va_list anything, char *s)
+{
+	printf("%s%f", s, va_arg(anything, int));
+}
+
+/**
+ * prints - prints a string
+ * @anything: string
+ * @s: pointer
+ *
+ */
+
+void print_s(va_list anything, char *s)
+{
+	char *a;
+
+	a = va_arg(anything, char *);
+	if (s == NULL)
+		s = "(nil)";
+
+	printf("%s%s", s, a);
+}
+
+/**
+ * print_all - prints anything
+ * @format: format
+ *
+ * Return: nothing
+ */
+
+void print_all(const char * const format, ...)
+{
+	va_list anything;
+	int a, b;
+	char *c;
+
+	fmt array[] = {{'c', printc},
+		       {'i', printi},
+		       {'f', print_f},
+		       {'s', print_s},
+	};
