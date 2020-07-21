f6a4e310c1552a563afa186adaa8539e23d47bbb
Matt Lopez <1833@holbertonschool.com>
2020-07-21 17:18:06 +0000

    edit to header

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,6 +4,10 @@
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void printc(va_list anything, char *s);
+void printi(va_list anything, char *s);
+void print_f(va_list anything, char *s);
+void print_s(va_list anything, char *s);
 void print_all(const char * const format, ...);
 
 
