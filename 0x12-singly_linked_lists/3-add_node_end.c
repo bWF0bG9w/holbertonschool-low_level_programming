99137af75a00466eb3f343d8fc604903fa0858ae
Matt Lopez <1833@holbertonschool.com>
2020-07-24 00:38:47 +0000

    adding 3

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+/**
+ * add_node_end - func that adds new node at the end of a list
+ * @head: pointer to pointer
+ * @str: string
+ *
+ * Return: address of new element or NULL if failed
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+
+
+}
