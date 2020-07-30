e1641e64e01d21e0a532bd519f22b225600a69d0
Matt Lopez <1833@holbertonschool.com>
2020-07-30 21:03:10 +0000

    adding 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: number
+ *
+ * Return: nothing
+ */
+
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+		print_binary(n >> 1);
+
+	else
+		_putchar (n + '0');
+
+}
